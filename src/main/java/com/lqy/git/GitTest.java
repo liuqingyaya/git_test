416a6b6701f668d17fec307ec8686cd5b4630b4b
liuqingyaya <liuqingyaya@163.com>
2022-08-31 03:12:49 +0800

    first commit

---
+++ src/main/java/com/lqy/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.lqy.git;
+
+/**
+ * @author Liuqy
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/8/313:05
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
